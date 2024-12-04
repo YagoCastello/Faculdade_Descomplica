f8a9a9601e9ed54fbb02012b9ff33d6e91e5f547
Yago Castello <yagodealmeidacs@gmail.com>
2024-12-04 14:25:07 -0300

    att

---
+++ aula_mod1.js
@@ -0,0 +1,35 @@
+console.log('testando');
+var a = 10;
+var b = 20;
+var total = a + b;
+console.log(total);
+
+var nomePessoa = 'Lucas';
+var NOMEPESSOA = 'JOSE';
+console.log(NOMEPESSOA)
+console.log(nomePessoa)
+
+/*usando virgula para declarar várias variáveis de uma vez */
+
+var var1 = 1,
+    var2 = 2,
+    var3 = 3;
+
+    console.log(var3)
+
+/* USO DO VAR, o var pode ser iniciado sem valor */
+
+var nome;
+console.log(nome)
+nome = 'yago'
+console.log(nome)
+
+/* USO DE LET, let precisa ser inicializado com um valor */
+let idade = 15;
+console.log(idade)
+
+/*USO DE CONST, const é um valor que não pode ser alterado durante a execuçaõ do script */
+const valor = 12;
+console.log(valor)
+valor = 15
+console.log(valor)
---
+++ aula_mod2.js
@@ -0,0 +1,12 @@
+var n = null;
+console.log(typeof(n));
+
+var nome = 'pedro';
+console.log(nome.toLocaleUpperCase());
+
+var cachorro ='nina';
+if(typeof(cachorro) == 'string'){
+    console.log(nome.toLocaleLowerCase)
+}else{
+    console.log('Você precusa usar uma string!')
+}
